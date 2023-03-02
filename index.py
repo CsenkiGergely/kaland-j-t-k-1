95d431840a8fbf1e8630bce1ee7ae8137db686d6
FesetoImre5 <feseto.imre@diak.szbi-pg.hu>
2023-03-02 12:24:07 +0100

    Changes and border in harc.py
    I'm thinking of a way I could implement the fighting mechanism-

--- index.py
+++ index.py
@@ -21,19 +21,3 @@ szoveg = kartyak["a"][e-1]
 
 print('\n')
 print(f"Pálya: {szoveg} \n")
-
-
-#Tanári json használata:
-#import json
-#
-#with open('kaland.json', 'r', encoding='utf-8') as f:
-#   data = json.load(f)
-#
-#print(data['bevezeto'])
-#
-#k = 0
-#
-#szoveg = data['kartyak'][k]['szoveg']
-#akcio = data['kartyak'][k]['akcio']
-#if akcio['tipus'] == 'ugras':
-#   print(szoveg)
