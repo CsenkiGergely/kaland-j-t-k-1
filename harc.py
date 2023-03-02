1f9d1e898514b8f6d5dec898c4eb8c033415bf9d
Csenki Gergely <118981063+CsenkiGergely@users.noreply.github.com>
2023-03-02 13:17:30 +0100

    harrc

--- harc.py
+++ harc.py
@@ -115,5 +115,42 @@ elif kérdés == 'nem':
     print('nem tetted próbára a szerencsédet')
     print(f'a te szerencséd {sajatluckmax}')
 
+class Tolvaj:
+    def __init__ (self, Thp, Tdmg):
+        self.Thp = Thp = 6
+        self.Tdmg = Tdmg = 7
+        
+class fohos:
+    def __init__ (self, sajathp, sajatdmg, sajatluckmax):
+        self.sajathp = sajathp = sum(dobas) + 12
+        self.sajatdmg = sajatdmg = dobas2 + 6
+        self.sajatluck = sajatluckmax = dobas3 + 6
+
+
+
+class harc:
+    def sebzés(self):
+        dobas5 = random.randint(1,6)
+        dobas6 = random.randint(1,6)
+        ertek1 = dobas5 =+ sajatdmg
+        ertek2 = dobas6 =+ self.Tdmg
+
+        if ertek2 > ertek1:
+            sajathp =- 2
+            print(f'Meg sebeztek. A te életerőd {sajathp}')
+        elif ertek2 < ertek1:
+            self.Thp -= 2
+            print(f'Megsebezted az ellenfelet. Az ellenfél életereje: {self.Thp}')
+        else:
+            print('Egyikőtök sem sebződött.')
+
+    def halal(self):
+        if sajathp <= 0:
+            print("A játéknak itt vége ")
+
+    def ellenfelhalal(self):
+        if self.Thp <= 0:
+            print("Meg ölted")
+
 
 
